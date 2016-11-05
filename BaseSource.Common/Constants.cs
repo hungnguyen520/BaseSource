262747dea6bbd782eec445aac47854239c60b143
Dam Quang Huy <dquanghuy@tma.com.vn>
2016-11-05 21:02:08 +0700

    Add more constant email contact

--- BaseSource.Common/Constants.cs
+++ BaseSource.Common/Constants.cs
@@ -9,5 +9,7 @@ namespace BaseSource.Common
     public static class Constants
     {
         public const string CONNECTION_STRING = "BaseSourceConnection";
+
+        public const string EMAIL_CONTACT = "nguyenphihung520@gmail.com";
     }
 }
