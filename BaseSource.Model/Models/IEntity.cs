64cb51bc874d75d9ebf35cee95b8a898f77fd6b2
nphihung <nphihung@tma.com.vn>
2016-11-07 17:38:25 +0700

    Add more model

--- BaseSource.Model/Models/IEntity.cs
+++ BaseSource.Model/Models/IEntity.cs
@@ -1,8 +1,12 @@
-﻿namespace BaseSource.Model.Models
+﻿using System;
+
+namespace BaseSource.Model.Models
 {
     public interface IEntity<TKey>
     {
         TKey Id { get; set; }
         bool IsDeleted { get; set; }
+        DateTime CreateDate { get; set; }
+        DateTime UpdateDate { get; set; }
     }
 }
---
+++ BaseSource.Model/Models/ProductCatalog.cs
@@ -0,0 +1,21 @@
+﻿using BaseSource.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseSource.Model.Models
+{
+    class ProductCatalog : IEntity<Guid>
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public string Description { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime UpdateDate { get; set; }
+        public string Owner { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
