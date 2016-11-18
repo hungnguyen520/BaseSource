e8048cc777230e307afbc4f3845551cf4c7a9d21
Nguyen Phi Hung <nphihung@tma.com>
2016-11-19 03:42:09 +0700

    Completed setup Identity P1

---
+++ BaseSource.Identity/Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace BaseSource.Identity.Infrastructure
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("IdentityConnection", throwIfV1Schema: false)
+        {
+            Configuration.ProxyCreationEnabled = false;
+            Configuration.LazyLoadingEnabled = false;
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
--- BaseSource.Identity/Properties/AssemblyInfo.cs
+++ BaseSource.Identity/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("bef1edec-1667-4a6c-a1b4-fcc6e710a3f8")]
+[assembly: Guid("ede4ec9e-8254-47da-adab-49561a76a113")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,8 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
